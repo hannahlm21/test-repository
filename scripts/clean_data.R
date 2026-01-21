cebcb7aa659f30b1658e4c41fb68ff193602f165
Hannahlm21 <hannahlmills10@gmail.com>
2026-01-21 01:05:08 -0700

    Add planes to flight data.

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -4,4 +4,5 @@ library(nycflights13)
 glimpse(flights)
 
 flights_airline <- flights |> 
-  left_join(airlines, by = c("carrier" = "carrier"))
+  left_join(airlines, by = c("carrier" = "carrier")) |> 
+  left_join(planes, by = c("tailnum" = "tailnum"))
